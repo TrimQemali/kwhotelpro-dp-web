36cb53bf484c823998e76857a74c4050aa23c2e6
trimqemali| <trimqemali@gmail.com>
2023-02-17 01:52:01 +0100

    Added Total Cost and Total paid values on hover reservation

--- backend_reservations.php
+++ backend_reservations.php
@@ -4,7 +4,7 @@ require_once '_db.php';
 $start = $_GET['start'];
 $end = $_GET['end'];
 
-$stmt = $db->prepare("SELECT RezerwacjaID, PokojID, DataOd, DataDo, Nazwisko, rezerwacje.Uwagi FROM banjisht.rezerwacje, banjisht.klienci where rezerwacje.KlientID=klienci.KlientID AND NOT ((DataDo <= :start) OR (DataOd >= :end))");
+$stmt = $db->prepare("SELECT RezerwacjaID, PokojID, DataOd, DataDo, Nazwisko, rezerwacje.Uwagi, Cena, Wplata FROM banjisht.rezerwacje, banjisht.klienci where rezerwacje.KlientID=klienci.KlientID AND NOT ((DataDo <= :start) OR (DataOd >= :end))");
 $stmt->bindParam(':start', $start);
 $stmt->bindParam(':end', $end);
 $stmt->execute();
@@ -24,9 +24,10 @@ foreach($result as $row) {
     $e->start = $row['DataOd'];
     $e->end = $row['DataDo'];
     $e->resource = $row['PokojID'];
-    $e->bubbleHtml = "Reservation details: <br/>".$e->id;
+    $e->cost = $row['Cena'];
+    $e->paid = $row['Wplata'];
+    $e->bubbleHtml = "Reservation ID:".$e->id."<br /> Total cost: ".$e->cost."<br /> Paid: ".$e->paid;
     
-    // additional properties
     //$e->status = $row['status'];
     //$e->paid = intval($row['paid']);
     $events[] = $e;
